b7afe488786c94ae1cb18620038142ed17cd42a0
lws <773286693@qq.com>
2019-09-30 18:00:46 +0800

    hahha

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="start">
+      <el-row :gutter="20">
+        <el-col :span="6" v-for="item in menus" :key="item.name">
+           <router-link :to="item.path" active-class="current">
+          <div class="grid-content bg-purple">
+              {{item.text}}
+              </div>
+              </router-link>
+        </el-col>
+      </el-row>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      menus: [
+        {
+          name: "home",
+          path: "/home",
+          text: "首页"
+        },
+        {
+          name: "cart",
+          path: "/cart",
+          text: "购物车"
+        },
+        {
+          name: "classification",
+          path: "/classification",
+          text: "分类"
+        },
+        {
+          name: "my",
+          path: "/my",
+          text: "我的"
+        }
+      ]
+    };
+  },
+  components: {}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style >
+a{text-decoration: none}
+.start {
+  position: fixed;
+  width: 100%;
+  height: 50px;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+}  .current{color:#f00;font-weight:bold;}
 </style>
---
+++ src/pages/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>gouwuche</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
